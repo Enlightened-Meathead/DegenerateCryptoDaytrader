a6ae029957b82cfdac8cea00362fabbcda523975
EnlightenedMeathead <91035070+Enlightened-Meathead@users.noreply.github.com>
2024-04-23 23:45:36 -0500

    Started the CLI user menu and did some brainstorming and outlining

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 ### Degenerate Crypto Daytrader
-
+This readme is a rough scratchpad until the program is working, so grammar and readability to the wind
 Intentions:
 - CLI based python app that you give user input to what crypto to buy and trade with given trade parameters and buy and sell protocols using a centralized exchange to do the trades then logsn and record the data of the trade for tax purposes
 - This is my first real programming project, so mainly the goal is to learn and just do even if something isn't optimal. If something isn't perfect, I don't care. I'm learning, and doing is better than doing nothing because I'm being neurotic about perfection 
@@ -19,11 +19,28 @@ To Do:
   - use initially to just get prices, see if you can then use ccxt for the buy and sell function on and exchange, then make a gui interface app so use a no kyc dekstop wallet like atomic
 - rsi scan interface
 
+#### CLI interface and help/man pages
+- create a function that checks for a key value passed to any option that triggers additional options and if found, set the corresponind option requirement to true
+- test and make sure if a one liner is done, the values are stored in the correct place and a dictionary of the values is created
+- for every option, create a check and logic that makes sure every value is the correct data type and within the specifiecd range
+- use regex to make sure the time format is correct and make the minutes and second 0-59
+- once the command line one-liners are functional, convert it to an interactive menu that gets all the required info
+- review page at the end of the menu that gives an option to edit any of the values
+- confirm the values for one liners or menu
+- have an option for one liners to force start and skip confirmation
+- GUI menu? yeah probably a good idea, do way later once everything is functional and working together
+- man page that deeply explains all options, also explains all functions and how they work as well as general guidance for noobs, do this later once everything works
 
-Possibly add modules for leveraged trading?
+End goal is have a one liner or cli menu that confirms the user input then passes that input to the main function
+- 
+
+
+End goal is have a one liner or cli menu that confirms the user input then passes that input to the main functio
+posssibly add modules for leveraged trading?
 
 ### Completed
 - basic buy and rsi based buy logic scan functions
 - basic sell and ladder ascending profit logic scan functions
 - profit loss allocators that tell what to buy back in with if swing trading and what to sell if profit harvesting
 - web socket based price scanner that returns the current spot price every 5 seconds for the selected asset.
+- 
--- user_input_menu.py
+++ user_input_menu.py
@@ -1,9 +1,8 @@
-# CLI based user input menu for people who don't know how to run the program with a one liner,
-# as well as a parser for a one liner of options to pass to the bot
+# CLI based user input menu for people who don't know how to run the program with a one-liner,
+# as well as a parser for a one-liner of options to pass to the bot
 import click
 import subprocess
 
-# Syntax will be (program name) --asset --buy_order_type --sell_order_type --
 '''
 Variables to be gathered from the user:
 --asset : the crypto to trade
@@ -54,6 +53,123 @@ $$$$$$  $    $   $     $   $    $ $    $ $$$$$  $$$$$$ $    $
 BEFORE USING THIS PROGRAM, READ THE DOCUMENTATION AND/OR MAN PAGE!
 ==================================================================
 """
+# Function to check if the user needs additional options based on primary option inputs
+def validate_additonal_options(ctx, param, value):
+    # If the value of the options is in a list of required options and that required option must have additional values,
+    # make the value of the next option it needs required.
+    additional_requirements = [
+        "basic_buy", "rsi", "basic_sell", "ladder", "profit_harvest", "swing_trade",
+    ]
+    if value is not None:
+
+# Function to check if the user passed no minimum capital or if they did enter a value convert it a float
+def validate_minimum_capital(ctx, param, value):
+    if value.lower() == "no":
+        return value.lower()
+    try:
+        return float(value)
+    except ValueError:
+        raise click.BadParameter("Minimum capital must be 'no' or a numeric value.")
+@click.command()
+@click.argument("menu",
+                type=click.Choice(["menu", "nomenu"]), default="menu",
+                help='Specifies to whether skip the interactive menu if you want to use an aliased one liner')
+@click.option("--bot_type",
+              type=click.Choice(["exchange", "atomic", "notifier"]),
+              help="Specify the type of bot")
+@click.option("--asset",
+              type=click.Choice(["bitcoin", "ethereum", "solana", "xrp","cardano", "dogecoin","shiba-inu", "monero"]),
+              help="Specify the crypto asset to buy and sell")
+@click.option("--capital",
+              type=click.Choice(["dollars", "tether", "usdc", "dai"]),
+              help="The type of capital you wish to buy the asset with and sell the asset for")
+@click.option("--start_type",
+              type=click.Choice(["buy", "sell", "previous"]),
+              help="The starting order type")
+@click.option("--buy_order_type",
+              type=click.Choice(["basic_buy", "rsi"]),
+              help="The type of buy order scan type you'd like to monitor the asset with to alert a buy signal")
+@click.option("--sell_order_type",
+              type=click.Choice(["basic_sell", "ladder"]),
+              help="The type of sell order scan type you'd like to  monitor the asset with to alert a sell signal")
+# Possibly refactor the name to stop_loss_limit later
+@click.option("--percent_loss_limit",
+              type=(float, int),
+              help="The percent you will allow your initial buy capital to drop by before selling at a loss"
+              )
+@click.option("--profit_loss_function",
+              type=click.Choice(["profit_harvest", "swing_trade"]),
+              help="The profit/loss reallocation protocol determining what to do with profits and losses post sell")
+@click.option("--initial_capital",
+              type=(float, int),
+              help="Specify the initial amount of capital you wish to place your buy order"
+              )
+@click.option("--minimum_capital",
+              callback=validate_minimum_capital,
+              help="Specify the minimum you want your capital to drop to before you stop trading or enter 'no' for no"
+                   "minimum")
+
+
+# Additional options that could become required
+@click.option("--basic_buy_price",
+              required=False,
+              type=(float, int),
+              help="The price of the asset you want to buy at as an integer or float"
+              )
+@click.option("--rsi_buy_number",
+              required=False,
+              type=(float, int),
+              help="The RSI you'd like to sell at if it stays at that RSI for the wait period")
+@click.option("--rsi_wait_period",
+              required=False,
+              # Use a time check function
+              type=click.Choice(),
+              help)
+@click.option("--basic_sell_profit",
+              required=False,
+              type=(int, float),
+              help="The percentage of profit you would like to achieve before the sell signal is true"
+              )
+
+@click.option("--minimum_ladder_profit",
+              required=False,
+              type=(float, int),
+              help="The minimum profit percent you want before the sell time begins for the ladder profit sell function"
+              )
+@click.option("--ladder_step_gain",
+              required=False,
+              type=(float, int),
+              help="The percentage of each step to reset the timer at once the minimum profit has been reached"
+              )
+@click.option("--ladder_step_loss",
+              required=False,
+              type=(float, int),
+              help="The percentage drop step that will trigger an immediate sell in the ladder function"
+              )
+@click.option("--ladder_timer_duration",
+              required=False,
+              help= "The duration of time to let the price stagnate before you sell. Format of 00:00:00 for hours, "
+                    "minutes, and seconds, minutes and seconds being between 0-59"
+              )
+              # use check time function
+@click.option("--ladder_step_sensitivity",
+              required=False,
+              type=(float, int),
+              help="The number to divide the step gain and loss percentage to make the number more sensitive the higher"
+                   "the profit goes"
+              )
+@click.option("--ladder_timer_sensitivity",
+              required=False,
+              help="The number to divide the ladder timer duration by to increase or decrease the time period to sell "
+                   "after"
+              )
+
+@click.option("--swing_trade_skim",
+              required=False,
+              type = (float, int),
+              help="The percentage of profit you want to skim off to keep and not use to be swing traded."
+              )
+
 
 
 def cli_menu_input(bot_type, asset, capital, start_type, buy_order_type, sell_order_type, profit_loss_function,
@@ -68,7 +184,7 @@ def test():
                             "automated trades on that exchange.\nAtomic will use the atomic wallet GUI with atomic "
                             "swaps for the trade.\nNotifier will just send notifications.",
                             type=click.Choice(["exchange", "atomic", "notifier"]))
-    # If bot type is exchange, display avaliable exchanges, have them enter an API key and neccesary creds
+    # If bot type is exchange, display available exchanges, have them enter an API key and necessary creds
     # If atomic, select resolution and tell them to run the test to make sure it will work.
     # If notifier, have them select how they would like to be notified: desktop, email?,sms?, some other notification?
     # For now, start with notifier and just send desktop notifications
@@ -80,12 +196,12 @@ def test():
     start_type = click.prompt("Select if you are starting with a buy, sell, or previous trade",
                               type=click.Choice(["buy", "sell", "previous"]))
     # If previous, lookup log of open buys from the spreadsheet and confirm which asset amount to use
-    buy_order_type = click.prompt("Select a buy order type", type=click.Choice(["basic", "rsi"]))
+    buy_order_type = click.prompt("Select a buy order type", type=click.Choice(["basic_buy", "rsi"]))
     # If basic, have the user input the price to buy the asset at. If RSI, have the user enter the RSI they want to
     # buy at, a wait period to buy after the RSI is still below the RSI they specified, to buy at if the wait period
     # expires, and the RSI drop limit
-    sell_order_type = click.prompt("Select a sell order type", type=click.Choice(["basic", "ladder"]))
-    # If basic, have the user enter the profit percentage to sell at and the stop loss percentage.
+    sell_order_type = click.prompt("Select a sell order type", type=click.Choice(["basic_sell", "ladder"]))
+    # If basic, have the user enter the profit percentage to sell at and the stop loss percentage
     # If ladder, have the user enter the in the minimum profit percent they want to sell at, the stop loss percent,
     # the positive step gain percentage, the negative step gain percentage, the timer duration to wait to see if the
     # price will stagnate then to sell, the step sensitivity value, and the timer sensitivity value.
@@ -94,7 +210,8 @@ def test():
     # If profit harvest, ask if they want a full sell or just the portion of the profits
     # If swing trade, ask what percentage of the profits they would like to skim off from the sells.
     initial_capital = click.prompt("How much capital you would like to buy in dollars")
-    minimum_capital = click.prompt("Would you like to set a minimum that you will allow your capital to dip that would trigger the bot to abort any further trades?"
+    minimum_capital = click.prompt("Would you like to set a minimum that you will allow your capital to dip that would "
+                                   "trigger the bot to abort any further trades?",
                                    type=click.Choice(["yes", "no"]))
 
     "Enter the lowest dollar amount you are willing to let your capital dip to before "
